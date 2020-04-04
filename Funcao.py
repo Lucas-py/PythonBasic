890e84695b295b700ffaeb5b9a59b24c11bebcdd
Lucas-py <50850273+Lucas-py@users.noreply.github.com>
2020-04-04 01:08:45 -0300

    Funções adicionando comentários
    Adicionado comentários para cada função no codigo

--- Funcao.py
+++ Funcao.py
@@ -1,17 +1,18 @@
 '''
 Iniciando funções em python
-
+-Criando duas funções,
+onde as duas serão imprimidas em uma outra função
 '''
 def Saudacao(ola):
-    return ola
+    return ola # função 'Saudacao' retornando paramêntro 'Ola'
 
 def Nomes(nome):
-    return nome
+    return nome # Função 'Nomes' retornando paramêntro 'Nome'
 
-def Imprimir(saudacao, nome):
+def Imprimir(saudacao, nome): #  Função onde sera imprimido as duas funções anteriores
     print(f'{saudacao}. sr {nome}')
 
-saud = Saudacao('Ola')
-nom = Nomes('Lucas')
+saud = Saudacao('Ola') #  Aplicando valor ao paramêntro 'Ola' da função 'Saudacao'
+nom = Nomes('Lucas') #  Aplicando o valor ao pramêntro 'nome' da função 'Nomes'
 
-Imprimir(saud,nom)
+Imprimir(saud,nom) #  passando os valores para  os paramentro e executando a função 'Imprimir'
